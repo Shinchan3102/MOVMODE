0f6dc3e548d79f61967b382f30be29ffb8f8d1fc
Ankit Kumar <ankitkumardos47@gmail.com>
2023-01-03 16:39:50 +0530

    added backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API=axios.create({baseURL:'http://localhost:5000/api'});
+const API=axios.create({baseURL:'https://movmode.vercel.app/api'});
 
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
